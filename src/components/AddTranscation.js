e813b63d825a080cc83fced60668e1d1a9e4cd47
aneesh072 <aneeshkarki292@gmail.com>
2022-08-07 20:18:47 +1000

    adding transaction successfully

--- src/components/AddTranscation.js
+++ src/components/AddTranscation.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const AddTranscation = () => {
   const [text, setText] = useState('');
-  const [amount, setAmount] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text: text,
+      amount: +amount,
+    };
+    console.log('good');
+    addTransaction(newTransaction);
+  };
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text"> Text</label>
           <input
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
   switch (action.type) {
     case 'DELETE_TRANSACTION':
@@ -6,6 +7,11 @@ export default (state, action) => {
         transactions: state.transactions.filter(
           (transaction) => transaction.id !== action.payload
         ),
+      };
+    case 'ADD_TRANSACTION':
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
       };
     default:
       return state;
